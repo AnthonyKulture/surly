6a638264940350afbc201703927d12375c32ead4
AnthonyKulture <contact@kulturecom.fr>
2026-01-08 17:58:56 +0100

    fix: use standard script for axeptio settings

--- components/ThirdPartyScripts.tsx
+++ components/ThirdPartyScripts.tsx
@@ -3,35 +3,38 @@
 import Script from "next/script";
 
 export function ThirdPartyScripts() {
-    return (
-        <>
-            {/* Axeptio Configuration */}
-            <Script id="axeptio-settings" strategy="afterInteractive">
-                {`
-          window.axeptioSettings = {
-            clientId: "690e0942b48e4a03c7b9b3f8",
-            cookiesVersion: "1dfd07d5-8c81-4f7c-8d76-dffc0f3160d3",
-            googleConsentMode: {
-              default: {
-                analytics_storage: "denied",
-                ad_storage: "denied",
-                ad_user_data: "denied",
-                ad_personalization: "denied",
-                wait_for_update: 500
+  return (
+    <>
+      {/* Axeptio Configuration */}
+      {/* Axeptio Configuration - Standard script for immediate init */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.axeptioSettings = {
+              clientId: "690e0942b48e4a03c7b9b3f8",
+              cookiesVersion: "1dfd07d5-8c81-4f7c-8d76-dffc0f3160d3",
+              googleConsentMode: {
+                default: {
+                  analytics_storage: "denied",
+                  ad_storage: "denied",
+                  ad_user_data: "denied",
+                  ad_personalization: "denied",
+                  wait_for_update: 500
+                }
               }
-            }
-          };
-        `}
-            </Script>
-            {/* Axeptio SDK */}
-            <Script
-                id="axeptio-script"
-                strategy="afterInteractive"
-                src="//static.axept.io/sdk.js"
-            />
+            };
+          `,
+        }}
+      />
+      {/* Axeptio SDK */}
+      <Script
+        id="axeptio-script"
+        strategy="afterInteractive"
+        src="//static.axept.io/sdk.js"
+      />
 
-            {/* Brevo Conversations Initialization */}
+      {/* Brevo Conversations Initialization */}
 
-        </>
-    );
+    </>
+  );
 }
